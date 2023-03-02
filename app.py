c1e57b0c1e4708cef91a55ef76043ea1f38bef4b
resnote <126734795+resnote@users.noreply.github.com>
2023-03-02 06:42:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from website import *
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python v' + sys.version.split()[0] + '\n'
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
+
+app=create_app()
